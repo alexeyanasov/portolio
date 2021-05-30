9cc54cc2a8b1b96c3b2c3fedf371c4af43087bb7
Alexey <alexey.anasov@yandex.by>
2021-05-30 19:33:18 +0300

    +tech +tooltip

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $(function () {
   $("#tabs").tabs({ active: 0 });
+  $(document).tooltip();
+  $(".tech__img").tooltip({
+    position: {
+      my: "center top",
+      at: "center bottom+20",
+      collision: "none",
+    },
+  });
 });
